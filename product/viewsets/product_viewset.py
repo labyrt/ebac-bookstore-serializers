4d2368e5e9e3d1a0384b5ed135e72f58f8553ef9
labyrt <labyrtcontact@gmail.com>
2026-08-14 12:24:39 -0300

    feat: cria ProductViewSet

---
+++ product/viewsets/product_viewset.py
@@ -0,0 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
+
+from product.models import Product
+from product.serializers import ProductSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all().prefetch_related("category").order_by("id")
+    serializer_class = ProductSerializer
