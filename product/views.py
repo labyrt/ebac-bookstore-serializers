ebd3ea12e2e4c5270a92309768ebf01aaf84ac9b
labyrt <labyrtcontact@gmail.com>
2026-08-14 12:23:23 -0300

    feat: adiciona viewsets de categoria e produto

--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all().order_by("id")
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all().prefetch_related("category").order_by("id")
+    serializer_class = ProductSerializer
