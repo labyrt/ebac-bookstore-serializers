0539d6c933cc57a2506184e942fe23ff3f60f5df
labyrt <labyrtcontact@gmail.com>
2026-08-14 12:24:14 -0300

    refactor: organiza viewsets em pacote dedicado

--- product/views.py
+++ product/views.py
@@ -1,14 +1,3 @@
-from rest_framework.viewsets import ModelViewSet
+from django.shortcuts import render
 
-from .models import Category, Product
-from .serializers import CategorySerializer, ProductSerializer
-
-
-class CategoryViewSet(ModelViewSet):
-    queryset = Category.objects.all().order_by("id")
-    serializer_class = CategorySerializer
-
-
-class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all().prefetch_related("category").order_by("id")
-    serializer_class = ProductSerializer
+# Create your views here.
