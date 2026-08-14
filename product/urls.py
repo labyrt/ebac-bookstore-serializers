65991fbde227fe3b58d8c85a197ffe102a298d86
labyrt <labyrtcontact@gmail.com>
2026-08-14 12:25:14 -0300

    refactor: usa pacote de viewsets nas rotas de product

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, ProductViewSet
+from .viewsets import CategoryViewSet, ProductViewSet
 
 
 router = DefaultRouter()
