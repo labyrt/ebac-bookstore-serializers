18b4ecb978640bccc2ece768090e10fcab3a5c3a
labyrt <labyrtcontact@gmail.com>
2026-08-15 21:28:50 -0300

    feat: adiciona rotas de verificação e webhook

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -2,11 +2,15 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+from bookstore.views import hello_world, update_server
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("product.urls")),
     path("api/", include("order.urls")),
+    path("hello/", hello_world, name="hello-world"),
+    path("update_server/", update_server, name="deploy-webhook"),
 ]
 
 if settings.DEBUG:
